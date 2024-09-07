bb3eab15e9ddb800da22738a2d473865a7185202
a0908931317 <a0908931317@gmail.com>
2024-09-08 02:00:16 +0800

    add spot

--- src/map/MapPage.js
+++ src/map/MapPage.js
@@ -1,10 +1,8 @@
-import {Story} from '../battle/story.js';
 // Map 頁面組件
 const MapPage = () => (
 	<div className="p-5">
 		<h1 className="text-2xl font-bold">Map Page</h1>
 		<p>This is the Map page content.</p>
-		<Story id={2}/>
 	</div>
 );
 
